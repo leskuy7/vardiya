ad6eed368e8d52443b5dd1c7934d35cc6c2a1ba9
leskuy7 <leskuy7@github.com>
2026-02-20 17:30:03 +0300

    fix: e2e test ayarlari ve css hatalari

--- shift-planner-api/test/app.e2e-spec.ts
+++ shift-planner-api/test/app.e2e-spec.ts
@@ -1,3 +1,4 @@
+/// <reference types="jest" />
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
